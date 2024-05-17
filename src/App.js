4f70954d72b041688d1618ab58a2e6a0e96586fa
VelivalaPraveen <168951291+VelivalaPraveen@users.noreply.github.com>
2024-05-17 22:37:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Game from "./Game";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
+
+export default App;
