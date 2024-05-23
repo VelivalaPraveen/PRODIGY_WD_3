675f43db834b8727a4bcc90dfed99df581a61ca5
VelivalaPraveen <168951291+VelivalaPraveen@users.noreply.github.com>
2024-05-23 10:15:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Game from "./Game";
 import "./index.css";
+import { GiTicTacToe } from "react-icons/gi";
 
 function App() {
   return (
     <>
+     <h1><GiTicTacToe size={40} />    Tic tac toe</h1>
       <Game />
     </>
   );
